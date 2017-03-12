b95ece7b1fc4ce708300923c96caf35b456bbe89
Aditi <aditipawde@live.com>
2017-03-12 19:12:52 +0530

    frames created

--- TimeTable1/dataAccessSQLAlchemy.py
+++ TimeTable1/dataAccessSQLAlchemy.py
@@ -3,17 +3,20 @@ import pymysql as py
 import pandas as pd
 import  entities as ent
 
-
+# Class for conencting to database
 class DB:
 
     def getConnection (self):
         engine = sql.create_engine('mysql+pymysql://root:Admin@123@localhost/timetable');
+
+        #print(engine.table_names());
         con = engine.connect();
         return con;
 
     def close (self,con):
         con.close();
 
+# Initialization method to initialize all frames
 def initialize (table_name):
     db = DB();
     
@@ -26,6 +29,14 @@ def initialize (table_name):
 
 
 print("welcome");
-frame = initialize('teacher');
 
-print(frame.head());
+# Required frames
+f_subject = initialize('subject');
+f_class = initialize('class');
+f_teacher = initialize('teacher');
+f_room = initialize('room');
+f_batch = initialize('batch');
+f_batchClass = initialize('batchclass'); #Name of table is batchClass,python is taking batchglass
+f_subjectClassTeacher = initialize('subjectclassteacher');
+
+print(f_subjectClassTeacher);
